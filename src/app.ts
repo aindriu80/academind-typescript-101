74ca55043fa1b73d4a87226b5f8fecdca8c41dc5
aindriu80 <aindriu80@gmail.com>
2024-02-08 13:42:10 +0000

    Using class-validator instead of types

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,34 @@
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
-import _ from "lodash";
+import { Product } from "./product.module";
 
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
 
-console.log(_.shuffle([1, 2, 3]));
+const products = [
+  { title: "A Carpet", price: 39.99 },
+  { title: "Another book", price: 23.33 },
+];
 
-declare var GLOBAL: any;
+const pl = new Product("A Book", 12.89);
 
-console.log(GLOBAL);
+const loadedProducts = products.map((prod) => {
+  return new Product(prod.title, prod.price);
+});
+
+const plainLoadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log("normal JavaScript ", prod.title, prod.price);
+}
+
+console.log("normal JavaScript ", pl.getInformation());
+
+for (const prod of plainLoadedProducts) {
+  console.log("placeToClass ", prod.getInformation());
+}
+console.log("plainToClass ", plainLoadedProducts);
